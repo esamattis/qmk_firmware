b0981e6c3ce0cf1fab9123473282932f16c69e48
Esa-Matti Suuronen <code@esamatti.fi>
2024-12-21 18:42:04 +0200

    [skip ci] WIP

--- keyboards/keebio/iris_ce/keymaps/esamattis/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/esamattis/keymap.c
@@ -5,6 +5,7 @@
 #include "keycodes.h"
 #include "keymap_us.h"
 #include "quantum_keycodes.h"
+#include "report.h"
 #include "send_string_keycodes.h"
 #include QMK_KEYBOARD_H
 
@@ -13,7 +14,8 @@
 enum custom_layers {
      _QWERTY,
      _LOWER,
-     _RAISE
+     _RAISE,
+     _MOUSE
 };
 
 enum custom_keycodes {
@@ -50,26 +52,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
-     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            /* */     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    QK_BOOT,
+     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            /* */     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
      _______,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            /* */     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
      KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            /* */     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-     KC_LSFT, KC_Z,    MT_X, MT_C, MT_V, KC_B, _______,            /* */     _______, KC_N, MT_M, MT_KOMM, MT_DOT, KC_SLSH, KC_RSFT,
-                                    KC_LGUI, MO(_LOWER), KC_ENT,   /* */     KC_SPC,  MO(_RAISE), KC_BACKSPACE
+     KC_LSFT, KC_Z,    MT_X, MT_C, MT_V, KC_B, MO(_MOUSE),            /* */    _______ , KC_N, MT_M, MT_KOMM, MT_DOT, KC_SLSH, KC_RSFT,
+                                    _______, MO(_LOWER), KC_ENT,   /* */     KC_SPC,  MO(_RAISE), KC_BACKSPACE
   ),
 
   [_LOWER] = LAYOUT(
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
-     KC_GRV,  LCAG(KC_Q), LCAG(KC_W),  LCAG(KC_E), _______, _______,                  _______, LSFT(KC_3),   LSFT(KC_BACKSLASH),   KC_P9,   KC_P0,   KC_PGDN,
-     KC_DEL,  LCAG(KC_A), LCAG(KC_S),  LCAG(KC_D), COPY, PASTE,                  KC_NUHS, LSFT(KC_SLASH),   LSFT(KC_2),   BACKTICK,   LSFT(KC_NUHS), KC_PIPE,
-     RM_NEXT, LCAG(KC_Z), LCAG(KC_X),  LCAG(KC_C), _______, KC_LCBR, KC_LPRN,         KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
-                                    _______, _______, KC_DEL,               KC_ESC,  _______, KC_P0
+     _______, _______, _______,   _______, _______,  _______,                  _______, _______, _______, _______, _______, QK_BOOT,
+     _______,  LCAG(KC_Q), LCAG(KC_W),  LCAG(KC_E), _______, _______,                  LALT(KC_7), LSFT(KC_3),   LSFT(KC_MINS),   LSFT(KC_1),   _______,   _______,
+     _______,  LCAG(KC_A), LCAG(KC_S),  LCAG(KC_D), COPY, PASTE,                  KC_NUHS, LSFT(KC_SLASH),   LSFT(KC_2),   BACKTICK,   LSFT(KC_NUHS), _______,
+     _______, LCAG(KC_Z), LCAG(KC_X),  LCAG(KC_C), _______, _______, _______,         _______, _______, ALGR(KC_RBRC),   LSFT(KC_5),   LSFT(KC_6),   _______, _______,
+                                    _______, _______, _______,               KC_ESC,  _______, _______
   ),
 
   [_RAISE] = LAYOUT(
-     KC_F12,  KC_F1,   KC_F2,   LALT(KC_8), LALT(KC_9)  ,   KC_F5,              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-     RM_TOGG, KC_EXLM, KC_AT,   RSFT(KC_8), RSFT(KC_9),  KC_PERC,               KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, QK_BOOT,
-     RM_NEXT, KC_MPRV, KC_MNXT, LSA(KC_8), LSA(KC_9), KC_UNDS,                  KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT, KC_HOME, KC_END,
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_GRV, RSFT(KC_GRV), KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_END,  RM_HUED, RM_SATD, RM_VALD, EE_CLR,
+     _______,  _______,   _______,   LALT(KC_8), LALT(KC_9)  ,   _______,         _______,   _______,   _______,   _______,   _______,  _______,
+     _______, _______, _______,   RSFT(KC_8), RSFT(KC_9),  LSFT(LALT(KC_7))/* \ */,               _______, _______,     _______,  _______, _______, _______,
+     _______, LALT(KC_2), _______, LSA(KC_8)/* { */, LSA(KC_9)/* } */, LSFT(KC_7)/* / */,                  KC_LEFT,  KC_DOWN, KC_UP, KC_RGHT, KC_HOME, KC_END,
+     _______, _______, _______, KC_GRV, RSFT(KC_GRV), LSFT(KC_0), _______,          _______, _______, _______,  _______, _______, _______, _______,
                                     _______, _______, _______,                   _______, _______, _______
+  ),
+
+  [_MOUSE] = LAYOUT(
+     _______,  _______,   _______,   _______, _______,   _______,         _______,                          _______,             _______,   _______,   _______,  _______,
+     _______, _______, _______,   _______, _______,  _______,               _______, _______,               QK_MOUSE_CURSOR_UP,  _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,                  QK_MOUSE_WHEEL_RIGHT,  QK_MOUSE_CURSOR_LEFT, QK_MOUSE_CURSOR_DOWN, QK_MOUSE_CURSOR_RIGHT, QK_MOUSE_WHEEL_LEFT, _______,
+     _______, _______, _______, _______, _______, _______, _______,          _______, QK_MOUSE_WHEEL_RIGHT, QK_MOUSE_WHEEL_UP,  QK_MOUSE_WHEEL_DOWN, QK_MOUSE_WHEEL_LEFT, _______, _______,
+                                    _______, _______, _______,                   MS_BTN1, MS_BTN2, _______
   )
 };
