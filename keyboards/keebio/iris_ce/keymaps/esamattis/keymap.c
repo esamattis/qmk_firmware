ac24668b1286f09f79e252fe1ffa4c12270814fd
Esa-Matti Suuronen <code@esamatti.fi>
2024-12-23 20:50:37 +0200

    [skip ci] WIP

--- keyboards/keebio/iris_ce/keymaps/esamattis/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/esamattis/keymap.c
@@ -52,6 +52,7 @@ bool caps_word_press_user(uint16_t keycode) {
         case KC_DEL:
         case KC_UNDS:
         case KC_LSFT:
+        case KC_RSFT:
             return true;
 
         default:
@@ -74,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            /* */     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-     _______,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            /* */     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     MO(_MOUSE),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            /* */     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BACKSPACE,
      KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            /* */     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      KC_LSFT, KC_Z,    MT_X, MT_C, MT_V, KC_B, MO(_MOUSE),            /* */    _______ , KC_N, MT_M, MT_KOMM, MT_DOT, KC_SLSH, KC_RSFT,
                                     _______, MO(_LOWER), KC_ENT,   /* */     KC_SPC,  MO(_RAISE), KC_BACKSPACE
